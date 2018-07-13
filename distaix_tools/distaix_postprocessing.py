85f4e221685d6b259fa7b1ab130b2a46ae8c1203
Felix Wege <felix.wege@rwth-aachen.de>
2018-07-13 17:41:52 +0200

    Realized id conversion in postprocessing script

--- distaix_tools/distaix_postprocessing.py
+++ distaix_tools/distaix_postprocessing.py
@@ -2,6 +2,7 @@
 
 import os
 import csv
+from shutil import copyfile
 
 ###############################################################################################
 ## Helper Functions                                                                          ##
@@ -9,7 +10,8 @@ import csv
 
 def modelicaToCSVConversion(path):
     root = os.path.splitext(path)[0]
-    os.rename(path, root + ".csv")
+    #os.rename(path, root + ".csv")
+    copyfile(path,root + ".csv")
 
     return root + ".csv"
 
@@ -17,36 +19,96 @@ def splitCSVFile(path):
 
     print('Splitting csv file...')
 
-    componentsLst = []
-    electricalGridLst = []
+    componentsList = []
+    electricalGridList = []
 
     with open(path, 'r') as csvfile:
-        saveVec = csv.reader(csvfile, delimiter=',')
+        saveVec = csv.reader(csvfile, delimiter = ',')
         for row in saveVec:
-            print(len(row))
+
             if not row:
                 pass
             elif row[0] == 'components.csv':
-                currentLst = componentsLst
+                currentList = componentsList
             elif row[0] == 'el_grid.csv':
-                currentLst = electricalGridLst
+                currentList = electricalGridList
             else:
-                currentLst.append(row)
+                currentList.append(row)
         
     with open('components.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        for component in componentsLst:
+        for component in componentsList:
             writer.writerow(component)
 
     with open('el_grid.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        for cable in electricalGridLst:
+        for cable in electricalGridList:
             writer.writerow(cable)
 
-def componentsIdConversion()
+def convertFile(path):
+    tempCSV = modelicaToCSVConversion(path)
+    splitCSVFile(tempCSV)
+    os.remove(tempCSV)
+
+def componentsIdConversion(componentsPath, idMappingDct):
+    
+    print('Converting component ids...')
+
+    componentsList = []
+
+    with open(componentsPath, 'r') as csvfile:
+        saveVec = csv.reader(csvfile, delimiter = ',')
+        for row in saveVec:
+            componentsList.append(row)
+
+    idCounter = 1
+    for component in componentsList:
+        if component[0] in idMappingDct.keys():
+            print("Error: ID %s is not unique!" % component[0])
+        else:
+            idMappingDct[component[0]] = idCounter
+            component[0] = idCounter
+            idCounter += 1
+    
+    with open(componentsPath, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for component in componentsList:
+            writer.writerow(component)
+
+def electricalGridIdConversion(elGridPath, idMappingDct):
+
+    print('Converting electrical grid ids...')
+
+    electricalGridList = []
+
+    with open(elGridPath, 'r') as csvfile:
+        saveVec = csv.reader(csvfile, delimiter = ',')
+        for row in saveVec:
+            electricalGridList.append(row)
+    
+    for cable in electricalGridList:
+        for x in range(0,2):
+            if cable[x] in idMappingDct.keys():
+                cable[x] = idMappingDct[cable[x]]
+            
+            else:
+                print("Error: ID %s does not relate to any component!" % cable[x])
+
+        if int(cable[1]) < int(cable[0]):
+            cable[0],cable[1] = cable[1],cable[0]
+
+    electricalGridList.sort(key=lambda k: k[0])
+    
+    with open(elGridPath, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for cable in electricalGridList:
+            writer.writerow(cable)
 
 ###############################################################################################
 ## Main Routine                                                                              ##
 ###############################################################################################
+idMappingDct = {}
 
-splitCSVFile(modelicaToCSVConversion("./test.mo"))
+convertFile('./default_output_name.mo')
+componentsIdConversion('./components.csv', idMappingDct)
+electricalGridIdConversion('./el_grid.csv', idMappingDct)
